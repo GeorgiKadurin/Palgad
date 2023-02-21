7c603d77fff19c41ccf55058611b87ae72856c0e
opilane <opilane@HP-CZC2349HT0>
2023-02-21 14:08:39 +0200

    Add project files.

---
+++ Palgad.py
@@ -0,0 +1,22 @@
+﻿from module1 import *
+
+#4 Kes ja kui palju saab kõige väiksemat palka.
+
+palgad = [1200, 2500, 750, 395, 1200]
+inimesed = ["A", "B", "C", "D", "E"]
+
+
+while True:
+
+     inimesed, palgad=kop(inimesed,palgad)
+     print(palgad)
+     print(inimesed)
+     break
+
+   
+log,palk=palg(palgad, inimesed)
+print(f"Väike palk saab {palk}, palk {log}")
+
+ 
+log,palk=spalg(palgad, inimesed)
+print(f"Suur palk saab {palk}, palk {log}")
---
+++ module1.py
@@ -0,0 +1,51 @@
+﻿def kop(inim:list, p:str)->str:
+    """
+    Määrme kop
+    :parem logt list, p str:Järjend kop...
+    :rtype: str
+    """
+    n=int(input("Kui palju inimesi: "))
+    for i in range(n):
+        print()
+        log=input("Sisesta nimi: ")
+        print()
+        j=int(input("Sisesta palk: "))
+        inim.append(log)
+        p.append(j)
+     
+    return inim,p
+
+
+
+
+
+
+
+
+
+
+
+def palg(palgad:list ,inimesed:list):
+    """
+    Määrme palg 
+    :parem palgad, inimesed ,p_min ,tehe ,:Järjend palg 
+    :rtype: str
+    """
+    palga=min(palgad)
+    index=palgad.index(palga)
+    pp=inimesed[index]
+    return palga, pp
+        
+
+
+
+def spalg(palgad:list ,inimesed:list):
+    """
+    Määrme palg
+    :parem palgad, inimesed ,p_min ,tehe ,:Järjend palg 
+    :rtype: str
+    """
+    palga=max(palgad)
+    index=palgad.index(palga)
+    pp=inimesed[index]
+    return palga, pp
