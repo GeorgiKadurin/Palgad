0a376bacf7e2a988af4e0b8307ba5807abbbdc3d
Georgi Kadurin <120027713+GeorgiKadurin@users.noreply.github.com>
2023-02-27 12:19:16 +0200

    Update module1.py

--- module1.py
+++ module1.py
@@ -1,4 +1,58 @@
-﻿def kop(inim:list, p:str)->str:
+def Sorteerimine(inimesed:list,palgad:list):
+    v=int(input("palk 1-kahaneb, 2-kasvab?"))
+    if v==1:
+        n=len(palgad)
+        for j in range(0,n-1):
+            for k in range(j+1,n):
+                if palgad[j] < palgad[k]:
+                    abi=palgad[j]
+                    palgad[j]=palgad[k]
+                    palgad[k]=abi
+                    abi=inimesed[j]
+                    inimesed[j]=inimesed[k]
+                    inimesed[k]=abi
+    elif v==2:
+         n=len(palgad)
+         for j in range(0,n-1):
+             for k in range(j+1,n):
+                 if palgad[j] > palgad[k]:
+                     abi=palgad[j]
+                     palgad[j]=palgad[k]
+                     palgad[k]=abi
+                     abi=inimesed[j]
+                     inimesed[j]=inimesed[k]
+                     inimesed[k]=abi
+
+
+
+
+
+
+
+
+
+
+def Kustutamine(inimesed:list,palk:list):
+    """Kirjendus..
+    :parem list inimesed, list palk: Inimesed ja palk järjend
+    :rtype: list, list
+    """
+    nimi=input("Sisesta nimi: ")
+    if nimi in inimesed:
+        n=inimesed.count(nimi)
+        for j in range(n):
+            ind=inimesed.index(nimi)
+            inimesed.pop(ind)
+            palk.pop(ind)
+
+
+
+
+
+
+
+
+def kop(inim:list, p:str)->str:
     """
     Määrme kop
     :parem logt list, p str:Järjend kop...
